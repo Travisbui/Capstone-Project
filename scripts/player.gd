3f2af31673e7014f2f92fe0c40711e7c39af2454
nichang1217 <nichang@s.sfusd.edu>
2024-03-06 12:42:13 -0800

    player animations added

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 const speed = 100
 var current_dir = "none"
 
+func _ready():
+	$AnimatedSprite2D.play("front_idle")
+
 func _physics_process(delta):
 	player_movement(delta)
 	
@@ -41,26 +44,26 @@ func play_anim(movement):
 	
 	if dir == "right":
 		anim.flip_h = false
-	if movement == 1:
-		anim.play("side_walk")
-	elif movement == 0:
-		anim.play("side_idle")
+		if movement == 1:
+			anim.play("side_walk")
+		elif movement == 0:
+			anim.play("side_idle")
 	if dir == "left":
 		anim.flip_h = true
-	if movement == 1:
-		anim.play("side_walk")
-	elif movement == 0:
-		anim.play("side_idle")
+		if movement == 1:
+			anim.play("side_walk")
+		elif movement == 0:
+			anim.play("side_idle")
 
 	if dir == "down":
-		anim.flip_h = false
-	if movement == 1:
-		anim.play("front_walk")
-	elif movement == 0:
-		anim.play("front_idle")
+		anim.flip_h = true
+		if movement == 1:
+			anim.play("front_walk")
+		elif movement == 0:
+			anim.play("front_idle")
 	if dir == "up":
-		anim.flip_h = false
-	if movement == 1:
-		anim.play("back_walk")
-	elif movement == 0:
-		anim.play("back_idle")
+		anim.flip_h = true
+		if movement == 1:
+			anim.play("back_walk")
+		elif movement == 0:
+			anim.play("back_idle")
