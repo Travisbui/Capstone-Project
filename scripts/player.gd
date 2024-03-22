b6b7505a11018e43ffdfd49ba11290a3df8c5979
Cabadga <juwu20@s.sfusd.edu>
2024-03-21 23:05:12 -0700

    box pushing complete, need deceleration

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,20 @@
 extends CharacterBody2D
 
-const speed = 100
+const speed = 50
 var current_dir = "none"
+var push_force = 80.0
 
 func _ready():
 	$AnimatedSprite2D.play("front_idle")
 
 func _physics_process(delta):
 	player_movement(delta)
+	move_and_slide()
+	
+	for i in get_slide_collision_count():
+		var c = get_slide_collision(i)
+		if c.get_collider() is RigidBody2D:
+			c.get_collider().apply_central_impulse(-c.get_normal() * push_force)
 	
 func player_movement(delta):
 	
