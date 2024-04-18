58d9a11351cfd2fde7cfe080bc14d976b7717419
Cabadga <juwu20@s.sfusd.edu>
2024-04-17 22:19:40 -0700

    spike trap finished

--- scripts/spike_trap.gd
+++ scripts/spike_trap.gd
@@ -1,12 +1,13 @@
 extends Area2D
 
 @export var spike_delay = 0.0
+@export var spike_integers = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	await get_tree().create_timer(spike_delay).timeout
 	$AnimationPlayer.play("spike_trigger")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_animation_player_animation_finished(spike_trigger):
+	await get_tree().create_timer(spike_integers).timeout
+	$AnimationPlayer.play("spike_trigger")
