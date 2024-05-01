e21357c7991c2848212e74c5e5e0aa0c686e91d7
Cabadga <juwu20@s.sfusd.edu>
2024-04-30 23:38:09 -0700

    all arrow traps finished

--- scripts/arrow_down.gd
+++ scripts/arrow_down.gd
@@ -7,7 +7,7 @@ var spawnPos : Vector2
 var spawnRot : float
 
 func _physics_process(delta):
-	global_position.x += speed * delta 
+	global_position.y += speed * delta 
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
