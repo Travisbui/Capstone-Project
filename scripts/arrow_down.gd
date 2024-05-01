c0d40a518b2dd3485875274c95acc831d9b819fb
Cabadga <juwu20@s.sfusd.edu>
2024-05-01 13:28:42 -0700

    traps added (some)

--- scripts/arrow_down.gd
+++ scripts/arrow_down.gd
@@ -13,4 +13,4 @@ func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
 
 func _on_body_entered(body: PhysicsBody2D):
-	print("dead")
+	queue_free()
--- scripts/arrow_left.gd
+++ scripts/arrow_left.gd
@@ -6,11 +6,14 @@ var direction : float
 var spawnPos : Vector2
 var spawnRot : float
 
+
 func _physics_process(delta):
 	global_position.x += -speed * delta 
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
 
-func _on_body_entered(body: PhysicsBody2D):
-	print("dead")
+func _on_body_entered(body):
+	if body is CharacterBody2D:
+		print("ded")
+		queue_free()
