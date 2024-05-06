73f8c156bde3fe49668adbc34d7206840aadfd60
Cabadga <juwu20@s.sfusd.edu>
2024-05-06 15:48:12 -0700

    clean up code

--- scripts/arrow_left.gd
+++ scripts/arrow_left.gd
@@ -6,7 +6,6 @@ var direction : float
 var spawnPos : Vector2
 var spawnRot : float
 
-
 func _physics_process(delta):
 	global_position.x += -speed * delta 
 
@@ -14,4 +13,4 @@ func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
 
 func _on_body_entered(body: PhysicsBody2D):
-		queue_free()
+	queue_free()
