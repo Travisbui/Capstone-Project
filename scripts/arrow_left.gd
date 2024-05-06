770ef1c973825fc07036135320eba5d44d844840
Cabadga <juwu20@s.sfusd.edu>
2024-05-06 15:39:16 -0700

    ded working

--- scripts/arrow_left.gd
+++ scripts/arrow_left.gd
@@ -13,7 +13,5 @@ func _physics_process(delta):
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
 
-func _on_body_entered(body):
-	if body is CharacterBody2D:
-		print("ded")
+func _on_body_entered(body: PhysicsBody2D):
 		queue_free()
--- scripts/ded.gd
+++ scripts/ded.gd
@@ -4,3 +4,10 @@ extends Control
 
 func _ready():
 	animated_sprite_2d.play("ded")
+	await get_tree().create_timer(1.75).timeout
+	$GameOver.visible = true
+	$MainMenu.visible = true
+	$MainMenu.grab_focus()
+
+func _on_main_menu_pressed():
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
