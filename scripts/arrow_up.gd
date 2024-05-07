ba0a16b9981506af7147efb1b1e04d323bcc5862
Cabadga <juwu20@s.sfusd.edu>
2024-05-07 15:07:42 -0700

    arrows fixed

--- scripts/arrow_up.gd
+++ scripts/arrow_up.gd
@@ -6,11 +6,15 @@ var direction : float
 var spawnPos : Vector2
 var spawnRot : float
 
+func _ready():
+	await get_tree().create_timer(0.15).timeout
+	$CollisionShape2D.disabled = false
+
 func _physics_process(delta):
 	global_position.y += -speed * delta 
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
 
-func _on_body_entered(body: PhysicsBody2D):
+func _on_body_entered(body):
 	queue_free()
